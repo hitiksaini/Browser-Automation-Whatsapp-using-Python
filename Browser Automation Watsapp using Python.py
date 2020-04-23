03eff4a11346d19442e6f4fb38c18cf97948ca76
Hitik Saini <56758122+hitiksaini@users.noreply.github.com>
2020-04-23 14:14:49 +0530

    Update Browser Automation Watsapp using Python.py

--- Browser Automation Watsapp using Python.py
+++ Browser Automation Watsapp using Python.py
@@ -2,19 +2,6 @@
 # coding: utf-8
 
 # In[ ]:
-
-
-from selenium import webdriver
-browser = webdriver.Chrome("C:\chromedriver")  #set the path to chrome-driver
-browser.get("https://www.google.com")  #this will open the url
-
-# elem= browser.find_element_by_link_text("Download")   this will find all the links in the website
-#elem.click()
-
-
-# In[ ]:
-
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -23,21 +10,14 @@ from selenium.webdriver.common.by import By
 import time
 
 browser = webdriver.Chrome("C:\chromedriver") 
-browser.get("https://web.whatsapp.com/")
-wait=WebDriverWait(browser,600)
-target='"Jaggu"'    #replace the EXACT name of your What's app contact name. 
-string="alright"    #this will be your message you want to send
+browser.get("https://web.whatsapp.com/")   #opens whats app in the browser , scan the QR CODE TO PROCEED.
+wait=WebDriverWait(browser,600)     #leave it as it is
+target='"my_friend_name"'    #replace the EXACT name of your What's app contact name. 
+string="retype your message here"    #this will be your message you want to send
 x_arg= '//span[contains(@title, ' +target + ')]'
 target = wait.until(ec.presence_of_element_located((By.XPATH, x_arg)))
 target.click()
 
 input_box= browser.find_element_by_class_name('_1Plpp')
-for i in range(150):
+for i in range(150):    #the number of messages (the message will be sent 150 times)
     input_box.send_keys(string+Keys.ENTER)
-
-
-# In[ ]:
-
-
-
-
